dd4063c253b63327191cd7a8bc9e4c5ce0ee9f08
Angel-Test1 <75077508+Angel-Test1@users.noreply.github.com>
2022-04-29 16:06:30 +0530

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -36,8 +36,8 @@ class Main extends React.Component {
 				<article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
 					<h2 className="major">Links</h2>
 					<ul className="icons">
-					<li><a href="/twitterr">
-							<FontAwesomeIcon icon={faTwitter} />
+					<li><a href="/coming-soon">
+							<FontAwesomeIcon icon={faDiscord} />
 						</a> Coming Soon... </li>
 					</ul>
 					{close}
